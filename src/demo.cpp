a26032e86a52df1ab00e213bb5590ccee22c769b
Vicc <wanghaoan.victor@gmail.com>
2018-10-02 09:59:15 +0200

    Update demo.cpp

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,30 +1,3 @@
-/*
- Copyright 2012. All rights reserved.
- Institute of Measurement and Control Systems
- Karlsruhe Institute of Technology, Germany
-
- Authors:
-
- libviso2 is free software; you can redistribute it and/or modify it under the
- terms of the GNU General Public License as published by the Free Software
- Foundation; either version 2 of the License, or any later version.
-
- libviso2 is distributed in the hope that it will be useful, but WITHOUT ANY
- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along with
- libviso2; if not, write to the Free Software Foundation, Inc., 51 Franklin
- Street, Fifth Floor, Boston, MA 02110-1301, USA
- */
-
-/*
- Documented C++ sample code of stereo visual odometry (modify to your needs)
- To run this demonstration, download the Karlsruhe dataset sequence
- '2010_03_09_drive_0019' from: www.cvlibs.net!
- Usage: ./viso2 path/to/sequence/2010_03_09_drive_0019
- */
-
 #include <iostream>
 #include <string>
 #include <vector>
